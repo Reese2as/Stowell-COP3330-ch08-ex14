a978cf1e48b5b97258d82cab3892b5e0f6d9e921
Reese Stowell <Reese2as@gmail.com>
2021-11-29 19:38:12 -0500

    a

--- Main.cpp
+++ Main.cpp
@@ -4,36 +4,32 @@
  */
 #include <iostream>
 #include "std_lib_facilities.h"
-class Name_value{
-	public:
-	int value;
-	string name;
-};
+
+/*
+
+Can we declare a non-reference function argument const (e.g., void f(const int);)?
+You can declare a non-reference function argument const but I dont beleive there is any good reason too.
+
+What might that mean?
+It means that the variable's value cannot be changed.
+
+Why might we want to do that?
+If you would like for the value of the variable to not change it makes some sense but the why not just have a global variable that doesnt change or just dont change the value in the function.
+
+Why don’t people do that often?
+Because it makes little sense too. The purpose of fuctions is usually to perform a task which usually invloves changing and returning a variable if you declare it as const you cant do that.
+*/
+
+void constfunc (const int num)
+{
+	num = 1; // When you declare it as const the variable is set and cannot be changed even in the function itself.
+}
+void func ( int num)
+{
+	num = 1; // When you declare it as non-const the variable's value to be changed throughout the function which has many uses as any programmer would know.
+}
 
 int main()
 {
-	Name_value input;
-	vector<Name_value> V;
-	int i = 0;
-	cout << "Please enter a name followed by a number to be added to the vector ";
-	while(cin >> input.name >> input.value) {	
 
-		if (input.name == "NoName" && input.value == 0){
-            break;
-		}
-		
-		for (i = 0; i < V.size(); i++){
-        if(input.name == V[i].name){
-			cout << "You have entered a duplicate name the program will now terminate ";
-			exit(0);
-			}
-		}
-		
-		V.insert(V.end(), input);
-		
-		cout << "Please enter another name followed by a number to be added to the vector ";
-	}
-	for (i = 0; i < V.size(); i++){
-        cout << V[i].name << " " << V[i].value << "\n";
-	}
 }
